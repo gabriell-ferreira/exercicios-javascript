4e9aa49ceb28b9ad17f4f1ae64d2376c055d2928
gabriell-ferreira <araujogabriel80@hotmail.com>
2020-10-28 18:05:10 -0400

    solução exercicio-8

---
+++ exercicio-08/script.js
@@ -0,0 +1,8 @@
+function mostrarcpf() {
+  let cpf = document.querySelector('#cpf');
+  if (cpf.value.length == 3 || cpf.value.length == 7){
+    cpf.value += '.'
+  } else if (cpf.value.length == 11){
+    cpf.value += '-'
+  }
+}
