136947897a4ffdf9b03daede1019f0f0e8441010
gabriell-ferreira <araujogabriel80@hotmail.com>
2020-10-29 14:28:21 -0400

    solução exericio-13

---
+++ exercicio-13/script.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector("#btn-soma")
+
+btn.addEventListener('click', () => {
+  let num1 = Number(document.getElementById('soma1').value)
+  let num2 = Number(document.getElementById('soma2').value)
+  
+  somar(num1, num2);
+})
+
+function somar(num1, num2) {
+  let resul = document.querySelector('#resultado-soma');
+
+  resul.value = num1 + num2
+}
+
