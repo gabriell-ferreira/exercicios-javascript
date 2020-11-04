c2bf4906b52632a91abfe307988a7be4c5bf7986
gabriell-ferreira <araujogabriel80@hotmail.com>
2020-11-03 21:17:14 -0400

    solução exercicio-35

---
+++ exercicios/exercicio-35/README.md
@@ -0,0 +1,2 @@
+# (37.g) Crie o seguinte array: var numbers = [5, 7, 1, 8, 9]
+E exiba de forma crescente @dica: .sort
---
+++ exercicios/exercicio-35/script.js
@@ -0,0 +1,5 @@
+let numbers = [5, 7, 1, 8, 9]
+
+document.write('nao ordenado: ' + numbers + '<br>')
+document.write('ordenado: ' + numbers.sort() + '<br>')
+document.write('descrescente: ' + numbers.reverse())
